934ebac9579b7b051c4bb559942101834e9961f6
Christian Meinhold <christian.meinhold@gmail.com>
2023-08-25 10:43:05 +0200

    Mark done

--- scripts.js
+++ scripts.js
@@ -7,13 +7,23 @@ function showDay(dayId) {
 
 document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
     checkbox.addEventListener('change', (e) => {
-      if (e.target.checked) {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 }
-        });
-      }
+        if (e.target.checked) {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 }
+            });
+        }
     });
-  });
-  
+});
+
+document.querySelectorAll('input[type="checkbox"]').forEach(function (checkbox) {
+    checkbox.addEventListener('change', function (e) {
+        const exerciseDiv = e.target.closest('.exercise');
+        if (e.target.checked) {
+            exerciseDiv.classList.add('done');
+        } else {
+            exerciseDiv.classList.remove('done');
+        }
+    });
+});
